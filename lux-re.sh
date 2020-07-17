7cc4d313b179ec6305a447b8c158e50dc69d3a6f
Josiah Schwab <jschwab@gmail.com>
2020-07-17 15:19:17 -0700

    Restart script for lux

---
+++ lux-re.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=sagb_ecsn
+#SBATCH --partition=cpuq
+#SBATCH --qos=cpuq
+#SBATCH --account=cpuq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --export=ALL
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jwschwab@ucsc.edu
+
+module load mesasdk/20.3.1
+export MESA_DIR=${DATA_DIR}/mesa-git
+
+export OMP_NUM_THREADS=36
+
+./re | tee re.out
+
+images_to_movie 'png/grid1*.png' 10M_sagb_ecsn_TPs.mp4
+
